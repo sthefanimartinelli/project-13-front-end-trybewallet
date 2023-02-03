4fa785c548292d0a7603728f3816146a17e4b57c
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2023-02-03 17:02:37 -0300

    Requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,18 @@ import React, { Component } from 'react';
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <>
+        <div>Table</div>
+        <th>Descrição</th>
+        <th>Tag</th>
+        <th>Método de pagamento</th>
+        <th>Valor</th>
+        <th>Moeda</th>
+        <th>Câmbio utilizado</th>
+        <th>Valor convertido</th>
+        <th>Moeda de conversão</th>
+        <th>Editar/Excluir</th>
+      </>
     );
   }
 }
